a485c9cb48f4b42b096b0193edafad998b9db5bd
Piotr Kasprowicz <kasprowiczpiotr@gmail.com>
2017-01-06 14:16:12 +0100

    message fix

--- sgm-dash/client/controllers/liveboard/liveboard.devicelist.js
+++ sgm-dash/client/controllers/liveboard/liveboard.devicelist.js
@@ -25,7 +25,7 @@ liveBoardApp.controller('LiveboardController',function($scope, $http, $timeout, 
       var updateChartAndTimestamp = function(measurement)
       {
         devChartList[measurement.devId].some(function(chartDescriptor){
-          if (chartDescriptor.values.includes(measurement.quantity))
+          if (chartDescriptor.values[measurement.quantity])
           {
             chartDescriptor.chart.appendMeasurement(measurement);
             return true;

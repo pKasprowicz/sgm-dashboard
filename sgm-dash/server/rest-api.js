0a8faddf7cef35a30f4696159d3cca02654e6ac2
Piotr Kasprowicz <kasprowiczpiotr@gmail.com>
2017-04-04 19:43:03 +0200

    1st draft

--- sgm-dash/server/rest-api.js
+++ sgm-dash/server/rest-api.js
@@ -15,7 +15,7 @@ var getWeather = function(callback)
         entries.forEach(function(entry)
         {
             console.log(entry);
-            console.log(entry.id);
+            console.log(entry.status);
             if(entry.status == "disabled")
             {
                 return;

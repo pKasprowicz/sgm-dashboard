85b8ab1d9ee02fc27ab0f0fc88b30cb00f6697db
Piotr Kasprowicz <kasprowiczpiotr@gmail.com>
2017-04-04 19:14:49 +0200

    1st draft

--- sgm-dash/server/routes.js
+++ sgm-dash/server/routes.js
@@ -101,7 +101,7 @@ module.exports = function(app)
       {
         collection.push(weatherData[id]);
       }
-      res.end(collection);
+      res.json(collection);
       //res.end(JSON.stringify(weatherData,null,2));
     });
   });
